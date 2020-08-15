3b811c7949e46c77205086689c9ab89b04ef8347
Shashank Sharma <shashanksharma@Shashanks-MBP.attlocal.net>
2020-08-15 15:09:21 -0500

    updated files to work with mac and linux OS

--- elk-stack/elkcerts/certgen.sh
+++ elk-stack/elkcerts/certgen.sh
@@ -4,10 +4,24 @@
 openssl genrsa -passout pass:"$1" -des3 -out rootCA.key 2048
 
 # Generate Root PEM (rootCA.pem) with 1024 days validity.
-openssl req -passin pass:"$1" -subj "/C=US/ST=Random/L=Random/O=Global Security/OU=IT Department/CN=Local Certificate"  -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.pem
+openssl req -passin pass:"$1" -subj "/C=US/ST=Random/L=Random/O=Global Security/OU=IT Department/CN=Local Certificate"  -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.pem -out rootCA.crt
 
 # Add root cert as trusted cert
-security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain rootCA.pem
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        # Linux
+        yum -y install ca-certificates
+        update-ca-trust force-enable
+        cp root.crt /etc/pki/ca-trust/source/anchors/
+        update-ca-trust
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+        security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain rootCA.pem
+else
+        # Unknown.
+        echo "Couldn't find desired Operating System. Exiting Now ......"
+        exit 1
+fi
+
 
 # Generate ES01 Cert
 openssl req -subj "/C=US/ST=Random/L=Random/O=Global Security/OU=IT Department/CN=localhost"  -new -sha256 -nodes -out es01.csr -newkey rsa:2048 -keyout es01.key
